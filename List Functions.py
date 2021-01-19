6fc0b904b2e6ac0a8cc2e67de3d7d298c9baebcd
Kathryn So <77600634+Kathrynms@users.noreply.github.com>
2021-01-19 14:57:59 -0700

    Add files via upload
    Follow along practice to the YouTube video Learn Python - Full Course for Beginners

---
+++ List Functions.py
@@ -0,0 +1,8 @@
+
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
+
+friends2 = friends.copy()
+lucky_numbers.reverse()
+print(lucky_numbers)
+print(friends2)
